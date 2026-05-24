a78994ad50e3aebd7df529a38a6cde0a267a5d93
ani1on <anilon5656@gmail.com>
2026-05-24 21:58:14 +0300

    preparing to send to the cloud v3

--- back/back_vet/api/core.py
+++ back/back_vet/api/core.py
@@ -3,15 +3,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
 origins = [
     "http://localhost:8080",             
     "https://clinicfastpig.netlify.app",      # ваш домен на Netlify
 ]
 import os
-print(f"PORT env: {os.environ.get('PORT')}")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
